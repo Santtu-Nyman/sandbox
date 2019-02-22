6d18b7fa0f985cc155dd3a0725cb2e3e42746098
unknown <s.nyman0616@gmail.com>
2019-02-22 18:22:55 +0200

    time

--- cwd/device_configuration.php
+++ cwd/device_configuration.php
@@ -2,10 +2,10 @@
 
 if (isset($_POST['id']))
 {
-	$device_id = trim(filter_var($_POST['id'], FILTER_SANITIZE_STRING));
-	$mode = '1';
-	$measurement_period = '60';
-	$target_temperature = '16';
+	$device_id = trim(filter_var($_POST['id'], FILTER_SANITIZE_STRING));// device id
+	$mode = '1';// 1 normal operating mode and 0 off
+	$measurement_period = '60';// time in seconds
+	$target_temperature = '16';// temperature in celsius
 	echo $device_id . ',' . $mode . ',' . $measurement_period . ',' . $target_temperature;
 }
 else
