e28f91989c803a2e8b3f3558b7aec343d483df5f
Santtu Nyman <s.nyman0616@gmail.com>
2019-11-04 14:21:46 +0200

    find master using python

--- robo_master/find_master.py
+++ robo_master/find_master.py
@@ -1,23 +1,25 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
-sock.bind(("0.0.0.0", 1732))
+def find_master():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
+	sock.bind(("0.0.0.0", 1732))
 
-message, master_address = sock.recvfrom(512)
-message = bytearray(message)
+	message, master_address = sock.recvfrom(512)
+	message = bytearray(message)
+	if len(message) < 16 or str(message[0:15]) != "TVT17SPL_MASTER"  :
+		return { "master_device_id" : -1, "master_device_address" : "0.0.0.0", "device_list" : [] }
+		
+	device_count = (len(message) - 16) / 5
+	configuration = { "master_device_id" : int(message[15]), "master_device_address" : str(master_address[0]), "device_list" : [] }
+	for i in range(device_count) :
+		device_ip = str(int(message[16 + (i * 5) + 3])) + "." + str(int(message[16 + (i * 5) + 2])) + "." + str(int(message[16 + (i * 5) + 1])) + "." + str(int(message[16 + (i * 5)]))
+		device_id = int(message[16 + (i * 5) + 4])
+		configuration["device_list"].append({ "ip" : device_ip, "id" : device_id })
+	return configuration
 
-if len(message) < 16 or str(message[0:15]) != "TVT17SPL_MASTER" :
-	exit()
-	
-master_id = int(message[15])
-device_count = (len(message) - 16) / 5
-devices = []
-for i in range(device_count) :
-	device_ip = int(message[16 + (i * 5)]) + (256 * int(message[16 + (i * 5) + 1])) + (65536 * int(message[16 + (i * 5) + 2])) + (16777216 * int(message[16 + (i * 5) + 3]))
-	device_id = int(message[16 + (i * 5) + 4])
-	devices.append({ "ip" : device_ip, "id" : device_id })
-
-print("Master device " + str(master_id) + " found at address " + str(master_address))
-for i in range(device_count) :
-	print("Device " + str(devices[i]["id"]) + " found at address " + str((devices[i]["ip"] / 16777216) % 256) + "." + str((devices[i]["ip"] / 65536) % 256) + "." + str((devices[i]["ip"] / 256) % 256) + "." + str(devices[i]["ip"] % 256) + "")
+configuration = find_master()
+print("Master device " + str(configuration["master_device_id"]) + " found at address " + configuration["master_device_address"])
+for i in range(len(configuration["device_list"])) :
+	print("Device " + str(configuration["device_list"][i]["id"]) + " found at address " + configuration["device_list"][i]["ip"] + "")
+exit()
